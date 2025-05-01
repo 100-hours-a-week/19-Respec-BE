493ddfe4116552aac120d18dd949f8db9b27bf94
jeli01 <jeli01@naver.com>
2025-05-01 22:46:40 +0900

    feat: 더미 유저 150명으로 변경

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/InitializeUser.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/InitializeUser.java
@@ -20,20 +20,15 @@ public class InitializeUser implements CommandLineRunner {
     public void run(String... args) {
         List<User> users = new ArrayList<>();
 
-        users.add(createUser("user1", "개발왕1", "https://picsum.photos/200"));
-        users.add(createUser("user2", "백엔드고수2", "https://picsum.photos/200"));
-        users.add(createUser("user3", "프론트엔드3", "https://picsum.photos/200"));
-        users.add(createUser("user4", "디자이너4", "https://picsum.photos/200"));
-        users.add(createUser("user5", "기획자5", "https://picsum.photos/200"));
-        users.add(createUser("user6", "마케터6", "https://picsum.photos/200"));
-        users.add(createUser("user7", "PM7", "https://picsum.photos/200"));
-        users.add(createUser("user8", "데이터분석가8", "https://picsum.photos/200"));
-        users.add(createUser("user9", "클라우드엔지니어9", "https://picsum.photos/200"));
-        users.add(createUser("user10", "보안전문가10", "https://picsum.photos/200"));
+        for (int i = 1; i <= 150; i++) {
+            String loginId = "user" + i;
+            String nickname = "userNickname" + i;
+            String profileUrl = "https://picsum.photos/200";
+
+            users.add(createUser(loginId, nickname, profileUrl));
+        }
 
         userRepository.saveAll(users);
-
-        System.out.println("10명의 사용자 데이터가 성공적으로 생성되었습니다.");
     }
 
     private User createUser(String loginId, String nickname, String profileUrl) {
