bc25ea80b1302c7c1fcefae55a331660eeee6414
jeli01 <jeli01@naver.com>
2025-05-07 17:29:39 +0900

    fix: entity 기본 값 설정을 수정

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/auth/entity/OAuth.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/auth/entity/OAuth.java
@@ -1,8 +1,17 @@
 package kakaotech.bootcamp.respec.specranking.domain.auth.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import kakaotech.bootcamp.respec.specranking.domain.common.type.OAuthProvider;
 import kakaotech.bootcamp.respec.specranking.domain.user.entity.User;
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +31,7 @@ public class OAuth {
     private User user;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "provider_name", nullable = false, columnDefinition = "VARCHAR(50) DEFAULT 'KAKAO'")
+    @Column(name = "provider_name", nullable = false, columnDefinition = "VARCHAR(50)")
     private OAuthProvider providerName;
 
     @Column(name = "provider_id", nullable = false, columnDefinition = "VARCHAR(255)")
--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/education/entity/Education.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/education/entity/Education.java
@@ -32,11 +32,11 @@ public class Education {
     private Spec spec;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "institute", nullable = false, columnDefinition = "VARCHAR(50) DEFAULT 'MIDDLE_SCHOOL'")
+    @Column(name = "institute", nullable = false, columnDefinition = "VARCHAR(50)")
     private Institute institute;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, columnDefinition = "VARCHAR(50) DEFAULT 'ENROLLED'")
+    @Column(name = "status", nullable = false, columnDefinition = "VARCHAR(50)")
     private FinalStatus status;
 
     public Education(Spec spec, Institute institute, FinalStatus status) {
