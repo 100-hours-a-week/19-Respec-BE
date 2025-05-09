4de8b967a88f76474cbb8967849f6553124b0f92
jeli01 <jeli01@naver.com>
2025-05-09 16:10:33 +0900

    feat: 개발 용이성을 위해 시큐리티 properties에 따라 등록 혹은 미등록 되도록 설정

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/UserUtils.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/UserUtils.java
@@ -1,12 +1,22 @@
 package kakaotech.bootcamp.respec.specranking.domain.user.util;
 
 import kakaotech.bootcamp.respec.specranking.domain.auth.dto.AuthenticatedUserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserUtils {
 
+    private static Boolean mockUserEnabled;
+
+    public void setFakeUserIdEnabled(@Value("${mock.login.user}") Boolean enabled) {
+        mockUserEnabled = enabled;
+    }
+
     public static Long getCurrentUserId() {
+        if (mockUserEnabled) {
+            return 1L;
+        }
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !(authentication.getPrincipal() instanceof AuthenticatedUserDto userDto)) {
