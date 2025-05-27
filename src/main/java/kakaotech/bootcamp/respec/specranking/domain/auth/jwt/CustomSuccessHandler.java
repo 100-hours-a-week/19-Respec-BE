2933b8a69e685db9299b96f7f64aad6f05eb7bf3
chokyoungjin <kyoungjin@kylie-13.local>
2025-05-27 20:00:16 +0900

    feat: refresh 토큰 구현

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/auth/jwt/CustomSuccessHandler.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/auth/jwt/CustomSuccessHandler.java
@@ -1,12 +1,14 @@
 package kakaotech.bootcamp.respec.specranking.domain.auth.jwt;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
+import kakaotech.bootcamp.respec.specranking.domain.auth.dto.AuthTokenRequestDto;
+import kakaotech.bootcamp.respec.specranking.domain.auth.dto.AuthTokenResponseDto;
 import kakaotech.bootcamp.respec.specranking.domain.auth.dto.CustomOAuth2User;
+import kakaotech.bootcamp.respec.specranking.domain.auth.service.AuthService;
 import kakaotech.bootcamp.respec.specranking.domain.user.entity.User;
 import kakaotech.bootcamp.respec.specranking.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,29 +24,34 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Value("${frontend.redirect-url}")
     private String frontendRedirectUrl;
 
-    private static final String TEMP_LOGIN_ID_COOKIE_NAME = "TempLoginId";
+    private static final String TEMP_LOGIN_ID = "TempLoginId";
+    private static final Long TEMP_LOGIN_ID_EXP = 1000L * 60 * 5; // 5분
+    private static final String ACCESS = "access";
+    private static final Long ACCESS_EXP = 1000L * 60 * 10; // 10분
 
+    private final AuthService authService;
     private final UserRepository userRepository;
-    private final JWTUtil jwtUtil;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
 
-        CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
-        String loginId = customUserDetails.getLoginId();
-
-        Optional<User> optUser = userRepository.findByLoginId(loginId);
+        CustomOAuth2User userInfo = (CustomOAuth2User) authentication.getPrincipal();
+        Optional<User> optUser = userRepository.findByLoginId(userInfo.getLoginId());
 
         if (optUser.isPresent()) {
             // 기존 사용자 - JWT 발급
             User user = optUser.get();
-            String token = jwtUtil.createJwts(user.getId(), user.getLoginId(), 1000L * 60 * 60 * 24);
-            CookieUtils.addCookie(response, "Authorization", token, 60 * 60 * 24);
+            AuthTokenRequestDto requestDto = new AuthTokenRequestDto(user.getId(), user.getLoginId());
+            AuthTokenResponseDto responseDto = authService.issueToken(requestDto, false);
+            authService.convertTokenToResponse(responseDto, response);
+
+            CookieUtils.addCookie(response, ACCESS, responseDto.accessToken(), (int) (ACCESS_EXP / 1000));
         } else {
             // 신규 사용자 - tempLoginId 쿠키 설정
-            String tmpLoginId = customUserDetails.getProvider() + "_" + customUserDetails.getProviderId();
-            CookieUtils.addCookie(response, TEMP_LOGIN_ID_COOKIE_NAME, tmpLoginId, 5 * 60);
+            String tmpLoginId = userInfo.getProvider() + "_" + userInfo.getProviderId();
+            CookieUtils.addCookie(response, TEMP_LOGIN_ID, tmpLoginId, (int) (TEMP_LOGIN_ID_EXP / 1000));
         }
 
         getRedirectStrategy().sendRedirect(request, response, frontendRedirectUrl);
