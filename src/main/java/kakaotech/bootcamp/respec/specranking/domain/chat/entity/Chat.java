f424d0c0f7ecadd763063da4ca384839c3ec3a53
jeli01 <jeli01@naver.com>
2025-06-04 17:19:19 +0900

    fix: Chatroom 관련하여 여러 클래스가 중복되어 있던 점 수정

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/entity/Chat.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/entity/Chat.java
@@ -1,8 +1,18 @@
 package kakaotech.bootcamp.respec.specranking.domain.chat.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kakaotech.bootcamp.respec.specranking.domain.chatroom.entity.Chatroom;
 import kakaotech.bootcamp.respec.specranking.domain.common.BaseTimeEntity;
 import kakaotech.bootcamp.respec.specranking.domain.user.entity.User;
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/repository/ChatroomRepository.java
+++
@@ -1,7 +0,0 @@
-package kakaotech.bootcamp.respec.specranking.domain.chat.repository;
-
-import kakaotech.bootcamp.respec.specranking.domain.chat.entity.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
-}
