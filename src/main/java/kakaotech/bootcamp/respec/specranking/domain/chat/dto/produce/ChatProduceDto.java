dea76774d692ef160d17a0f697ed6336c4233252
jeli01 <jeli01@naver.com>
2025-06-05 14:31:41 +0900

    refactor: ChatProduceDto 생성자 방식으로 수정
    setter 또는 builder의 경우 밑으로 길게 늘어나 가독성이 좋지 않다.
    생성자 방식으로 수정한다.

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/dto/produce/ChatProduceDto.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/dto/produce/ChatProduceDto.java
@@ -1,12 +1,20 @@
 package kakaotech.bootcamp.respec.specranking.domain.chat.dto.produce;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class ChatProduceDto {
     private String idempotentKey;
     private String senderId;
     private String receiverId;
     private String content;
     private String status;
+
+    public ChatProduceDto(String idempotentKey, String senderId, String receiverId, String content, String status) {
+        this.idempotentKey = idempotentKey;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.content = content;
+        this.status = status;
+    }
 }
