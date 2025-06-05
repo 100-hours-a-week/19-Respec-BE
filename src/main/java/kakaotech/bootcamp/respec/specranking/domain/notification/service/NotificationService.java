06ad79d7aa8bb893a3991f2dcba2ced93c3c2317
jeli01 <jeli01@naver.com>
2025-06-06 03:39:31 +0900

    feat: session 정보에 없는 유저에 관한 요청이 왔을 경우 처리

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/notification/service/NotificationService.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/notification/service/NotificationService.java
@@ -1,9 +1,29 @@
 package kakaotech.bootcamp.respec.specranking.domain.notification.service;
 
+import static kakaotech.bootcamp.respec.specranking.domain.common.type.NotificationTargetType.CHAT;
+
+import kakaotech.bootcamp.respec.specranking.domain.notification.entity.Notification;
+import kakaotech.bootcamp.respec.specranking.domain.notification.repository.NotificationRepository;
+import kakaotech.bootcamp.respec.specranking.domain.user.entity.User;
+import kakaotech.bootcamp.respec.specranking.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final UserRepository userRepository;
+    private final NotificationRepository notificationRepository;
+
+    public void createChatNotification(Long receiverId) {
+        User receiver = userRepository.findById(receiverId)
+                .orElseThrow(() -> new RuntimeException("User not found: " + receiverId));
+
+        Notification notification = new Notification(receiver, CHAT, receiver.getId());
+        notificationRepository.save(notification);
+    }
+
 }
