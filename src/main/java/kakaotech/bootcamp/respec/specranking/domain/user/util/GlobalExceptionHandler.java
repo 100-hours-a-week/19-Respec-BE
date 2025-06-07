06ddf63dad6c2ec4bf4c582cc0f538dee3740e48
chokyoungjin <kyoungjin@kylie-13.local>
2025-06-07 17:48:55 +0900

    feat: 회원정보 수정 기능 관련 전역예외처리 추가

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/GlobalExceptionHandler.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/user/util/GlobalExceptionHandler.java
@@ -1,12 +1,41 @@
 package kakaotech.bootcamp.respec.specranking.domain.user.util;
 
+import kakaotech.bootcamp.respec.specranking.global.dto.SimpleResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<SimpleResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .findFirst()
+                .map(ObjectError::getDefaultMessage)
+                .orElse("유효성 검사 실패");
+
+        SimpleResponseDto response = new SimpleResponseDto(false, errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<SimpleResponseDto> handleIllegalArgumentException(IllegalArgumentException ex) {
+        SimpleResponseDto response = new SimpleResponseDto(false, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<SimpleResponseDto> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        SimpleResponseDto response = new SimpleResponseDto(false, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
     // 닉네임 중복 예외 처리
     @ExceptionHandler(DuplicateNicknameException.class)
