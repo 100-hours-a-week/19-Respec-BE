7e24113b4e4497c209565cb9b40214d5ad5a937c
chokyoungjin <kyoungjin@kylie-13.local>
2025-06-08 23:45:25 +0900

    feat: 즐겨찾기 목록 조회 기능 구현

---
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/bookmark/dto/BookmarkListResponse.java
@@ -0,0 +1,82 @@
+package kakaotech.bootcamp.respec.specranking.domain.bookmark.dto;
+
+import kakaotech.bootcamp.respec.specranking.domain.bookmark.entity.Bookmark;
+import kakaotech.bootcamp.respec.specranking.domain.common.type.JobField;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BookmarkListResponse {
+    private Boolean isSuccess;
+    private String message;
+    private BookmarkListData data;
+
+    @Data
+    public static class BookmarkListData {
+        private List<BookmarkItem> bookmarks;
+        private Boolean hasNext;
+        private String nextCursor;
+
+        public BookmarkListData(List<BookmarkItem> bookmarks, Boolean hasNext, String nextCursor) {
+            this.bookmarks = bookmarks;
+            this.hasNext = hasNext;
+            this.nextCursor = nextCursor;
+        }
+    }
+
+    @Data
+    public static class BookmarkItem {
+        private Long id;
+        private SpecInfo spec;
+
+        public BookmarkItem(Long id, SpecInfo spec) {
+            this.id = id;
+            this.spec = spec;
+        }
+    }
+
+    @Data
+    public static class SpecInfo {
+        private Long id;
+        private String nickname;
+        private String profileImageUrl;
+        private Double score;
+        private Long totalRank;
+        private Long totalUserCount;
+        private Long jobFieldRank;
+        private Long jobFieldUserCount;
+        private JobField jobField;
+        private Boolean isBookmarked;
+        private Long commentsCount;
+        private Long bookmarksCount;
+
+        public SpecInfo(Long id, String nickname, String profileImageUrl, Double score,
+                        Long totalRank, Long totalUserCount, Long jobFieldRank, Long jobFieldUserCount,
+                        JobField jobField, Boolean isBookmarked, Long commentsCount, Long bookmarksCount) {
+            this.id = id;
+            this.nickname = nickname;
+            this.profileImageUrl = profileImageUrl;
+            this.score = score;
+            this.totalRank = totalRank;
+            this.totalUserCount = totalUserCount;
+            this.jobFieldRank = jobFieldRank;
+            this.jobFieldUserCount = jobFieldUserCount;
+            this.jobField = jobField;
+            this.isBookmarked = isBookmarked;
+            this.commentsCount = commentsCount;
+            this.bookmarksCount = bookmarksCount;
+        }
+    }
+
+    public BookmarkListResponse(Boolean isSuccess, String message, BookmarkListData data) {
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static BookmarkListResponse success(List<BookmarkItem> bookmarks, Boolean hasNext, String nextCursor) {
+        BookmarkListData data = new BookmarkListData(bookmarks, hasNext, nextCursor);
+        return new BookmarkListResponse(true, "즐겨찾기 목록 조회 성공", data);
+    }
+}
