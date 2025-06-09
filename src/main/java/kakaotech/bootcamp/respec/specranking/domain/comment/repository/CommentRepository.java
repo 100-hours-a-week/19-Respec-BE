dd8b5d63575d32cc9bc745fd1a68552f8e4ef913
chokyoungjin <kyoungjin@kylie-13.local>
2025-06-09 15:29:20 +0900

    feat: 댓글 작성 기능 구현

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/comment/repository/CommentRepository.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/comment/repository/CommentRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.repository.query.Param;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT COUNT(c) FROM Comment c WHERE c.spec.id = :specId")
     Long countBySpecId(@Param("specId") Long specId);
+
+    @Query("SELECT COALESCE(MAX(c.bundle), 0) FROM Comment c WHERE c.spec.id = :specId")
+    Integer findMaxBundleBySpecId(@Param("specId") Long specId);
 }
