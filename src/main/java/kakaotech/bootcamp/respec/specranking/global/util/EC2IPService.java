05d46ca008b20cd09743e7852d4d37108ff844b9
jeli01 <jeli01@naver.com>
2025-06-19 16:11:29 +0900

    fix: ec2 private ip application.properties로 받도록 변경

--- src/main/java/kakaotech/bootcamp/respec/specranking/global/util/EC2IPService.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/global/util/EC2IPService.java
@@ -1,49 +1,20 @@
 package kakaotech.bootcamp.respec.specranking.global.util;
 
-import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @Profile("!local")
 @RequiredArgsConstructor
 public class EC2IPService implements IPService {
 
-    private final WebClient ec2MetadataWebClient;
-    private final ServletWebServerApplicationContext webServerContext;
+    @Value("${server.private-address}")
+    private String serverPrivateAddress;
 
     public String loadEC2PrivateAddress() {
-        return fetchPrivateIPWithIMDSv2() + ":" + getPrivatePort();
+        return serverPrivateAddress;
     }
 
-    private String fetchPrivateIPWithIMDSv2() {
-        String token = fetchIMDSToken();
-
-        return ec2MetadataWebClient
-                .get()
-                .uri("http://169.254.169.254/latest/meta-data/local-ipv4")
-                .header("X-aws-ec2-metadata-token", token)
-                .retrieve()
-                .bodyToMono(String.class)
-                .timeout(Duration.ofSeconds(3))
-                .block();
-    }
-
-    private String fetchIMDSToken() {
-        return ec2MetadataWebClient
-                .put()
-                .uri("http://169.254.169.254/latest/api/token")
-                .header("X-aws-ec2-metadata-token-ttl-seconds", "21600")
-                .retrieve()
-                .bodyToMono(String.class)
-                .timeout(Duration.ofSeconds(3))
-                .block();
-    }
-
-    private int getPrivatePort() {
-        return webServerContext.getWebServer().getPort();
-    }
 }
