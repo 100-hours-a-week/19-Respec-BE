417091f32ef7a87000029096f4608d3bb9b903f1
jeli01 <jeli01@naver.com>
2025-06-19 14:50:50 +0900

    fix: EC2MetaData url 픽스

--- src/main/java/kakaotech/bootcamp/respec/specranking/global/config/EC2MetaDataConfig.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/global/config/EC2MetaDataConfig.java
@@ -8,12 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 @Profile("!local")
 public class EC2MetaDataConfig {
-    private static final String EC2_METADATA_BASE_URL = "http://169.254.169.254/latest/meta-data";
 
     @Bean
     public WebClient ec2MetadataWebClient() {
         return WebClient.builder()
-                .baseUrl(EC2_METADATA_BASE_URL)
+                .baseUrl("")
                 .build();
     }
 }
