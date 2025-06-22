04889684f51c26705a164f61d4fb28cc7cead98e
jeli01 <jeli01@naver.com>
2025-06-22 17:46:55 +0900

    refactor: global dto record 전환

--- src/main/java/kakaotech/bootcamp/respec/specranking/global/dto/SimpleResponseDto.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/global/dto/SimpleResponseDto.java
@@ -1,15 +1,7 @@
 package kakaotech.bootcamp.respec.specranking.global.dto;
 
-import lombok.Data;
-
-@Data
-public class SimpleResponseDto {
-    private Boolean isSuccess;
-    private String message;
-
-    public SimpleResponseDto(Boolean isSuccess, String message) {
-        this.isSuccess = isSuccess;
-        this.message = message;
-    }
+public record SimpleResponseDto(
+        Boolean isSuccess,
+        String message
+) {
 }
-
