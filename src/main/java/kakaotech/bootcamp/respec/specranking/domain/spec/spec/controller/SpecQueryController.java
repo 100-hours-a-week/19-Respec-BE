e902450d0670d1ade6253df59a500c7e05c2bf88
jeli01 <jeli01@naver.com>
2025-06-23 16:03:21 +0900

    chore: prod 문제해결을 위한 로깅

--- src/main/java/kakaotech/bootcamp/respec/specranking/domain/spec/spec/controller/SpecQueryController.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/spec/spec/controller/SpecQueryController.java
@@ -9,6 +9,7 @@ import kakaotech.bootcamp.respec.specranking.domain.spec.spec.service.SpecDetail
 import kakaotech.bootcamp.respec.specranking.domain.spec.spec.service.SpecQueryService;
 import kakaotech.bootcamp.respec.specranking.global.common.type.JobField;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/specs")
 @RequiredArgsConstructor
+@Slf4j
 public class SpecQueryController {
 
     private final SpecQueryService specQueryService;
@@ -49,6 +51,7 @@ public class SpecQueryController {
 
     @GetMapping("/{specId}")
     public SpecDetailResponse getSpecDetail(@PathVariable Long specId) {
+        log.info("Get Spec Detail Query for Spec ID: {}", specId);
         return specDetailQueryService.getSpecDetail(specId);
     }
 
