bc658819c27bd8bb72fc69bcf0d933cea0328075
Jaemin <61683188+jeli01@users.noreply.github.com>
2025-06-30 16:25:24 +0900

    Revert "feat: 커서 기반 페이지네이션 랭킹 캐시 적용"

--- src/main/java/kakaotech/bootcamp/respec/specranking/global/common/config/CacheConfig.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/global/common/config/CacheConfig.java
@@ -25,7 +25,6 @@ public class CacheConfig {
 
         cacheConfigurations.put("specMetadata", createCacheConfig(Duration.ofHours(2)));
         cacheConfigurations.put("specDetails", createCacheConfig(Duration.ofMinutes(3)));
-        cacheConfigurations.put("top10Rankings", createCacheConfig(Duration.ofSeconds(30)));
 
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(createCacheConfig(Duration.ofMinutes(30)))
