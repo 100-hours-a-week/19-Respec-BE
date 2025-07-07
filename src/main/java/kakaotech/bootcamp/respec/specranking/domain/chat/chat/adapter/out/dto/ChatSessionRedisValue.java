49ccc6879489aadbc42928cf37669ca276c203ea
jeli01 <jeli01@naver.com>
2025-07-08 01:33:16 +0900

    fix: 레디스에 접속한 채팅방의 정보 (상대 정보)도 추가하여 넣도록 수정

---
+++ src/main/java/kakaotech/bootcamp/respec/specranking/domain/chat/chat/adapter/out/dto/ChatSessionRedisValue.java
@@ -0,0 +1,7 @@
+package kakaotech.bootcamp.respec.specranking.domain.chat.chat.adapter.out.dto;
+
+public record ChatSessionRedisValue(
+        String privateAddress,
+        Long partnerId
+) {
+}
