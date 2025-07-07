85ac9af14d4e216d18f87aa9b914d91474c2eccf
chokyoungjin <kyoungjin@kylie-13.local>
2025-07-07 22:46:29 +0900

    test: 댓글 CUD 기능 service단 테스트코드 작성

--- src/test/java/kakaotech/bootcamp/respec/specranking/fixture/CommentFixture.java
+++ src/test/java/kakaotech/bootcamp/respec/specranking/fixture/CommentFixture.java
@@ -1,4 +1,39 @@
 package kakaotech.bootcamp.respec.specranking.fixture;
 
+import kakaotech.bootcamp.respec.specranking.domain.social.comment.entity.Comment;
+import org.mockito.Mockito;
+
+import static kakaotech.bootcamp.respec.specranking.fixture.TestConstants.*;
+import static org.mockito.Mockito.lenient;
+
 public class CommentFixture {
+
+    public static Comment createMockComment() {
+        return createMockComment(DEFAULT_COMMENT_ID, DEFAULT_COMMENT_CONTENT, ROOT_COMMENT_DEPTH, null);
+    }
+
+    public static Comment createMockParentComment() {
+        return createMockComment(DEFAULT_PARENT_COMMENT_ID, DEFAULT_COMMENT_CONTENT, ROOT_COMMENT_DEPTH, null);
+    }
+
+    public static Comment createMockReply() {
+        return createMockComment(REPLY_ID, REPLY_CONTENT, REPLY_DEPTH, DEFAULT_PARENT_COMMENT_ID);
+    }
+
+    public static Comment createMockComment(Long commentId, String content, Integer depth, Long parentCommentId) {
+        Comment comment = Mockito.mock(Comment.class);
+        lenient().when(comment.getId()).thenReturn(commentId);
+        lenient().when(comment.getContent()).thenReturn(content);
+        lenient().when(comment.getDepth()).thenReturn(depth);
+        lenient().when(comment.getParentCommentId()).thenReturn(parentCommentId);
+        return comment;
+    }
+
+    public static Comment createMockNonRootComment() {
+        Comment comment = createMockReply();
+        lenient().when(comment.isRootComment()).thenReturn(false);
+        return comment;
+    }
+
+    private CommentFixture() { }
 }
--- src/test/java/kakaotech/bootcamp/respec/specranking/fixture/TestConstants.java
+++ src/test/java/kakaotech/bootcamp/respec/specranking/fixture/TestConstants.java
@@ -11,15 +11,28 @@ public class TestConstants {
     public static final String ANOTHER_USER_PROFILE_URL = "https://example.com/another-profile.jpg";
 
     public static final Long DEFAULT_SPEC_ID = 1L;
+    public static final Long NON_EXISTENT_SPEC_ID = 999L;
 
     public static final Long DEFAULT_COMMENT_ID = 2L;
+    public static final Long DEFAULT_PARENT_COMMENT_ID = 3L;
+    public static final Long REPLY_ID = 4L;
+    public static final Long NON_EXISTENT_COMMENT_ID = 999L;
 
     public static final String DEFAULT_COMMENT_CONTENT = "테스트 댓글 내용";
+    public static final String REPLY_CONTENT = "대댓글 내용";
+    public static final String UPDATED_COMMENT_CONTENT = "수정된 댓글 내용";
+    public static final String FIRST_COMMENT_CONTENT = "첫 번째 댓글";
 
     public static final String DEFAULT_CREATED_AT = "2025-01-01T10:00:00";
     public static final String DEFAULT_UPDATED_AT = "2025-01-01T10:00:00";
     public static final String REPLY_CREATED_AT = "2025-01-01T11:00:00";
     public static final String REPLY_UPDATED_AT = "2025-01-01T11:00:00";
+
+    public static final Integer INITIAL_BUNDLE_NUMBER = 1;
+    public static final Integer EXISTING_MAX_BUNDLE_NUMBER = 5;
+
+    public static final Integer ROOT_COMMENT_DEPTH = 0;
+    public static final Integer REPLY_DEPTH = 1;
 
     private TestConstants() { }
 }
